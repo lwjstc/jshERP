9f3b2b63eb787bdc9a755e1a37987dd171f60471
lwjstc <51664113@qq.com>
2022-02-06 01:10:31 +0800

    modify boot config

--- jshERP-boot/src/main/bin/run-manage.sh
+++ jshERP-boot/src/main/bin/run-manage.sh
@@ -8,7 +8,7 @@ readonly PID_FILE="$LOGS_HOME/application.pid"
 readonly APP_MAIN_CLASS="jshERP.jar"
 readonly LOG_CONFIG="$CONFIG_HOME/logback-spring.xml"
 readonly JAVA_RUN="-Dlogs.home=$LOGS_HOME -Dlogging.config=$LOG_CONFIG -Dspring.config.location=file:$CONFIG_HOME -Dspring.pid.file=$PID_FILE -Dspring.pid.fail-on-write-error=true"
-readonly JAVA_OPTS="-server -Xms128m -Xmx320m -XX:PermSize=128M -XX:MaxPermSize=256M $JAVA_RUN"
+readonly JAVA_OPTS="-server -Xms128m -Xmx320m $JAVA_RUN"
 readonly JAVA="java"
 PID=0
 if [ ! -x "$LOGS_HOME" ]
